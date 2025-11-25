a2da62299d8e29fb47fee08b718db12924d05924
Lucky-Blessed <blessednkwor7@gmail.com>
2025-11-24 18:11:55 -0700

    More update

--- app.js
+++ app.js
@@ -4,9 +4,23 @@ require('dotenv').config();
 const port = process.env.PORT || 8080;
 const mainRoutes = require('./routes/mainRoutes');
 
+const userRoutes = require('./routes/users');
+const ejs = require('ejs');
+
+// set view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Static files
+app.use(express.static('public'));
+
+// Parse incoming request bodies
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 // Middlewares
 app.use(mainRoutes);
+app.use(userRoutes);
 
 
 app.listen(port, ()=>{
