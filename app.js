9fdc19bab91b89bacdc1a1d11695b912c9e7bd42
Lucky-Blessed <blessednkwor7@gmail.com>
2025-11-26 18:39:46 -0700

    Creating Json database and utils

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 const mainRoutes = require('./routes/mainRoutes');
-
 const userRoutes = require('./routes/users');
 const ejs = require('ejs');
 
---
+++ utils/file.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('fs').promises;
+const filePath = require('..,database.json');
+
+async function readData(){
+    try {
+        const data = await fs.readFile(filePath, "utf-8");
+        return JSON.parse(data),
+        
+    } catch (error) {
+        res.status(500).send(`Internal Server Error: ${error.message}`);
+        
+    }
+}
+
+async function writeData(){
+    try {
+        await fs.writeFile(filePath, JSON.stringify(data, null, 2));        
+    } catch (error) {
+        res.status(500).send(`Internal Server Error: ${error.message}`);
+        
+    }
+}
+
+module.exports = {
+    readData,
+    writeData
+
+}
